bb1f6d12ed3f7c2c75ad12665fdbd34c2b2aecf4
StaseysKo <staseys@staseys.ru>
2024-02-20 13:30:59 +0200

    3

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Grid, Container, Typography, Stack, Box, Button } from '@mui/material';
 
 export default function Home() {
   return (
