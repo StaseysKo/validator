c7343e46a0208f14be35a8e0b695c720a3cd1744
StaseysKo <staseys@staseys.ru>
2024-02-20 13:48:12 +0200

    10

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <Typography> тест </Typography>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
