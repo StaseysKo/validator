c3ed345591394d606a2e1e49278817991e0ef7e0
StaseysKo <staseys@staseys.ru>
2024-02-20 13:33:15 +0200

    4

--- src/app/page.js
+++ src/app/page.js
@@ -25,6 +25,7 @@ export default function Home() {
               height={24}
               priority
             />
+            <Typography> тест </Typography>
           </a>
         </div>
       </div>
