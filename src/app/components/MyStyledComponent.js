21c61333411c55e6ecc77d6ec4feba36fea3bb3e
StaseysKo <113985975+StaseysKo@users.noreply.github.com>
2024-02-22 14:35:32 +0200

    2

--- src/app/components/MyStyledComponent.js
+++
@@ -1,8 +0,0 @@
-// В файле, например, app/components/MyStyledComponent.js
-import styled from '@emotion/styled';
-
-const MyStyledComponent = styled.div`
-  color: hotpink;
-`;
-
-export default MyStyledComponent;
--- src/createEmotionCache.js
+++
@@ -1,8 +0,0 @@
-import createCache from '@emotion/cache';
-
-export default function createEmotionCache() {
-  const cache = createCache({ key: 'css' });
-  console.log(cache); // Выведет объект кэша в консоль
-  console.log('ну, хуй'); // еще выведет в консоль слово хуй
-  return cache;
-}
