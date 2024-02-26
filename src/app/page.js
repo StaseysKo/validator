6538b88b6bfc401a441bfa2974c4f3a1a5f153d8
StaseysKo <113985975+StaseysKo@users.noreply.github.com>
2024-02-26 11:49:48 +0200

    4

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Image from "next/image";
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
     <main className={styles.main}>
