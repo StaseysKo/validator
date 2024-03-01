d2db09abeb6c76be0e9418f374079ae1e7482e4c
StaseysKo <113985975+StaseysKo@users.noreply.github.com>
2024-03-01 13:52:48 +0200

    Удалил неиспользуемые зависимости в _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,7 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 // scroll bar
 import 'simplebar/src/simplebar.css';
 
 // lightbox
-/* eslint-disable import/no-unresolved */
 import 'yet-another-react-lightbox/styles.css';
 import 'yet-another-react-lightbox/plugins/captions.css';
 import 'yet-another-react-lightbox/plugins/thumbnails.css';
@@ -23,17 +20,10 @@ import 'src/styles/iconsStyle.css';
 import 'src/styles/logoStyle.css';
 
 // ----------------------------------------------------------------------
-
-import PropTypes from 'prop-types';
 // next
 import Head from 'next/head';
-// @mui
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
-import { LocalizationProvider } from '@mui/x-date-pickers';
 // theme
 import ThemeProvider from 'src/theme';
-// components
-import ProgressBar from 'src/components/ProgressBar';
 
 // ----------------------------------------------------------------------
 import CallBackButton from 'src/components/CallBackButton';
@@ -42,49 +32,8 @@ import CallBackButton from 'src/components/CallBackButton';
 
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 
-App.propTypes = {
-  Component: PropTypes.func,
-  pageProps: PropTypes.object,
-};
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    // Функция инициализации Яндекс.Метрики
-    const initYandexMetrika = () => {
-      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-      m[i].l=1*new Date();
-      var j=0;
-      while(j < e.scripts.length){if (e.scripts[j].src === r) { return; } j++; }
-      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-      ym(96137092, "init", {
-          defer: true,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true,
-          webvisor:true
-      });
-    };
-
-    // Вызов функции инициализации при первой загрузке
-    initYandexMetrika();
-
-    // Функция для отслеживания событий маршрутизации
-    const handleRouteChange = (url) => {
-      ym(96137092, 'hit', url);
-    };
-
-    // Подписка на события изменения маршрута
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    // Отписка от событий при размонтировании компонента
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
@@ -94,13 +43,11 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1, width=device-width, user-scalable=no" />
       </Head>
 
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <ThemeProvider>
-          <ProgressBar />
-          <CallBackButton />
-          {getLayout(<Component {...pageProps} />)}
-        </ThemeProvider>
-      </LocalizationProvider>
+
+      <ThemeProvider>
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
+
     </AppRouterCacheProvider>
   );
 }
