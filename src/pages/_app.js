387d678c4148f6871074195d70def96b41922478
StaseysKo <113985975+StaseysKo@users.noreply.github.com>
2024-03-01 13:58:21 +0200

    Удалил лишние зависимости

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,3 @@
-// scroll bar
-import 'simplebar/src/simplebar.css';
-
-// lightbox
-import 'yet-another-react-lightbox/styles.css';
-import 'yet-another-react-lightbox/plugins/captions.css';
-import 'yet-another-react-lightbox/plugins/thumbnails.css';
-
-// slick-carousel
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
-// lazy image
-import 'react-lazy-load-image-component/src/effects/blur.css';
-import 'react-lazy-load-image-component/src/effects/opacity.css';
-import 'react-lazy-load-image-component/src/effects/black-and-white.css';
-
 import 'src/styles/globals.css';
 import 'src/styles/iconsStyle.css';
 import 'src/styles/logoStyle.css';
